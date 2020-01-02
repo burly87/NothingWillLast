74721ab4c1023baa6049a5cbf0a8e5c9eee31dd4
Forve <tobias.forve@gmx.de>
2020-01-02 20:03:42 +0100

    Basics
    - Movement added
    - Deactivated Colliders

---
+++ Nothing Will Last/Assets/Scripts/EnterHole.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterHole : MonoBehaviour
+{
+    public int LayerOnEnter;
+    public int LayerOnExit;
+
+   void OnTriggerEnter(Collider other)
+   {
+       if(other.gameObject.tag == "Building")
+       {
+           other.gameObject.layer = LayerOnEnter;
+       }
+   }
+
+//    void OnTriggerExit(Collider other)
+//    {
+//        if(other.gameObject.tag == "Building")
+//        {
+//            other.gameObject.layer = LayerOnExit;
+//        }
+//         // can activate Calculation hier 
+//       // Destroy(other.gameObject);
+//       print("Exit Collider, get points and destroy me later");
+//    }
+}
---
+++ Nothing Will Last/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float walkSpeed = 0.75f;
+
+    public float turnSmoothTime = 0.2f;
+    private float turnSmoothVelocity;
+
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x , inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+        float targetSpeed = inputDir.magnitude;
+        //currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed);
+        this.transform.Translate(transform.forward * targetSpeed * Time.deltaTime, Space.World);
+    }
+
+}
